d432db7667886f5ce5215e4165e5405c42e6e10d
Florian Pose <fp@igh-essen.com>
2011-12-09 10:46:04 +0100

    Minor.

--- master/master.h
+++ master/master.h
@@ -242,8 +242,8 @@ struct ec_master {
                                       ext_datagram_queue. */
 
     struct list_head external_datagram_queue; /**< External Datagram queue. */
-    unsigned int send_interval; /**< Interval between calls to
-                                  ecrt_master_send */
+    unsigned int send_interval; /**< Interval between two calls to
+                                  ecrt_master_send(). */
     size_t max_queue_size; /**< Maximum size of datagram queue */
 
     unsigned int debug_level; /**< Master debug level. */
