5536cb65ff5c3ba696e20b212e8017ab7b21ece2
Gavin Lambert <uecasm@users.sourceforge.net>
2014-05-31 18:08:27 +1200

    Mark download data as const. Remove redundant code. [07-sdo-up-download]

--- tool/CommandDownload.cpp
+++ tool/CommandDownload.cpp
@@ -171,16 +171,17 @@ void CommandDownload::execute(const StringVector &args)
             throwCommandException(err);
         }
         data.data_size = contents.size();
-        data.data = new uint8_t[data.data_size + 1];
+        uint8_t *sdo_data = new uint8_t[data.data_size + 1];
+        data.data = sdo_data;
 
         try {
             data.data_size = interpretAsType(
-                    dataType, contents, data.data, data.data_size);
+                    dataType, contents, sdo_data, data.data_size);
         } catch (SizeException &e) {
-            delete [] data.data;
+            delete [] sdo_data;
             throwCommandException(e.what());
         } catch (ios::failure &e) {
-            delete [] data.data;
+            delete [] sdo_data;
             err << "Invalid value for type '" << dataType->name << "'!";
             throwInvalidUsageException(err);
         }
@@ -192,16 +193,17 @@ void CommandDownload::execute(const StringVector &args)
             data.data_size = DefaultBufferSize;
         }
 
-        data.data = new uint8_t[data.data_size + 1];
+        uint8_t *sdo_data = new uint8_t[data.data_size + 1];
+        data.data = sdo_data;
 
         try {
             data.data_size = interpretAsType(
-                    dataType, args[valueIndex], data.data, data.data_size);
+                    dataType, args[valueIndex], sdo_data, data.data_size);
         } catch (SizeException &e) {
-            delete [] data.data;
+            delete [] sdo_data;
             throwCommandException(e.what());
         } catch (ios::failure &e) {
-            delete [] data.data;
+            delete [] sdo_data;
             err << "Invalid value argument '" << args[valueIndex]
                 << "' for type '" << dataType->name << "'!";
             throwInvalidUsageException(err);
