e3c7edb27c874ec6e1432c1fe9ba97a5e9c039b0
Florian Pose <fp@igh-essen.com>
2014-10-28 16:54:59 +0100

    Added all drivers for kernel 3.16.

--- devices/update.sh
+++ devices/update.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-KERNELDIR=/data/kernel/linux-3.14.12
-PREVER=3.12
-KERNELVER=3.14
+KERNELDIR=/data/kernel/linux-3.16.6
+PREVER=3.14
+KERNELVER=3.16
 
 for f in $KERNELDIR/drivers/net/ethernet/{realtek/8139too,realtek/r8169,intel/e100}.c; do
     echo $f
