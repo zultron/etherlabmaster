cc5735bf3025e2276d0257f24a256f2478d035ef
Sascha Ittner <sascha.ittner@modusoft.de>
2021-06-17 16:13:38 +0200

    Fix RX buffer handling of Intel IGB driver (memory leak)

--- devices/igb/igb_main-4.19-ethercat.c
+++ devices/igb/igb_main-4.19-ethercat.c
@@ -8486,33 +8486,23 @@ static int igb_clean_rx_irq(struct igb_q_vector *q_vector, const int budget)
 
 		rx_buffer = igb_get_rx_buffer(rx_ring, size);
 
-		if (adapter->ecdev) {
-			unsigned char *va = page_address(rx_buffer->page) + rx_buffer->page_offset;
-			unsigned int size = le16_to_cpu(rx_desc->wb.upper.length);
-			ecdev_receive(adapter->ecdev, va, size);
-			adapter->ec_watchdog_jiffies = jiffies;
-			igb_reuse_rx_page(rx_ring, rx_buffer);
-		}
-		else {
-			/* retrieve a buffer from the ring */
-			if (skb)
-				igb_add_rx_frag(rx_ring, rx_buffer, skb, size);
-			else if (ring_uses_build_skb(rx_ring))
-				skb = igb_build_skb(rx_ring, rx_buffer, rx_desc, size);
-			else
-				skb = igb_construct_skb(rx_ring, rx_buffer,
-							rx_desc, size);
+                /* retrieve a buffer from the ring */
+                if (skb)
+                        igb_add_rx_frag(rx_ring, rx_buffer, skb, size);
+                else if (ring_uses_build_skb(rx_ring))
+                        skb = igb_build_skb(rx_ring, rx_buffer, rx_desc, size);
+                else
+                        skb = igb_construct_skb(rx_ring, rx_buffer,
+                                                rx_desc, size);
 
-			/* exit if we failed to retrieve a buffer */
-			if (!skb) {
-				rx_ring->rx_stats.alloc_failed++;
-				rx_buffer->pagecnt_bias++;
-				break;
-			}
+                /* exit if we failed to retrieve a buffer */
+                if (!skb) {
+                        rx_ring->rx_stats.alloc_failed++;
+                        rx_buffer->pagecnt_bias++;
+                        break;
+                }
 
-			igb_put_rx_buffer(rx_ring, rx_buffer);
-		}
-
+                igb_put_rx_buffer(rx_ring, rx_buffer);
 		cleaned_count++;
 
 		/* fetch next buffer in frame if non-eop */
@@ -8520,23 +8510,25 @@ static int igb_clean_rx_irq(struct igb_q_vector *q_vector, const int budget)
 			continue;
 
 		if (adapter->ecdev) {
-			total_packets++;
-			continue;
+			ecdev_receive(adapter->ecdev, skb->data, size);
+			adapter->ec_watchdog_jiffies = jiffies;
+			total_bytes += skb->len;
+			dev_kfree_skb_any(skb);
+		} else {
+			/* verify the packet layout is correct */
+			if (igb_cleanup_headers(rx_ring, rx_desc, skb)) {
+				skb = NULL;
+				continue;
+			}
+
+			/* probably a little skewed due to removing CRC */
+			total_bytes += skb->len;
+
+			/* populate checksum, timestamp, VLAN, and protocol */
+			igb_process_skb_fields(rx_ring, rx_desc, skb);
+
+			napi_gro_receive(&q_vector->napi, skb);
 		}
-
-		/* verify the packet layout is correct */
-		if (igb_cleanup_headers(rx_ring, rx_desc, skb)) {
-			skb = NULL;
-			continue;
-		}
-
-		/* probably a little skewed due to removing CRC */
-		total_bytes += skb->len;
-
-		/* populate checksum, timestamp, VLAN, and protocol */
-		igb_process_skb_fields(rx_ring, rx_desc, skb);
-
-		napi_gro_receive(&q_vector->napi, skb);
 
 		/* reset skb pointer */
 		skb = NULL;
