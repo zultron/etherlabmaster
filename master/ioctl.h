571fcc0f2fb6b00b1527385c24ccc6cd5c82f2c9
John Morris <john@zultron.com>
2023-04-28 17:54:15 -0500

    master/ioctl.h:  Bump version magic

--- master/ioctl.h
+++ master/ioctl.h
@@ -48,7 +48,7 @@
  * Increment this when changing the ioctl interface!
  */
 
-#define EC_IOCTL_VERSION_MAGIC 36
+#define EC_IOCTL_VERSION_MAGIC 40
 
 // Command-line tool
 #define EC_IOCTL_MODULE                EC_IOR(0x00, ec_ioctl_module_t)
