b792d3bddd9449d507a9a52468093f89a10e4185
Florian Pose <fp@igh.de>
2026-03-25 09:55:32 +0100

    Removed compile-time assert.

--- master/master.c
+++ master/master.c
@@ -65,7 +65,6 @@
 
 #define smp_store_release(p, v) \
 do { \
-	compiletime_assert_atomic_type(*p); \
 	smp_mb(); \
 	ACCESS_ONCE(*p) = (v); \
 } while (0)
@@ -73,7 +72,6 @@ do { \
 #define smp_load_acquire(p)	\
 ({ \
 	typeof(*p) ___p1 = ACCESS_ONCE(*p); \
-	compiletime_assert_atomic_type(*p); \
 	smp_mb(); \
 	___p1; \
 })
